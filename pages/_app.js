c2f028bf419e992a8d466dc1fdc3f29799c52dd8
Yasser Mahmoud <43951500+yaser55v@users.noreply.github.com>
2023-02-05 17:31:33 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,11 @@
 import { useEffect, useState } from "react";
-import SplashScreen from "../components/SplashScreen";
+
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <SplashScreen {...pageProps} />
+      
       <Component {...pageProps} />
     </>
   );
